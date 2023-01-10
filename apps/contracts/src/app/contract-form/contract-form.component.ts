42d51934d52fa43467f49cc2099736138a487546
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2023-01-10 15:45:19 -0500

    add contract form markup and class props

--- apps/contracts/src/app/contract-form/contract-form.component.ts
+++ apps/contracts/src/app/contract-form/contract-form.component.ts
@@ -3,6 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contract-form',
   templateUrl: './contract-form.component.html',
-  styleUrls: ['./contract-form.component.css'],
+  styleUrls: ['./contract-form.component.scss']
 })
-export class ContractFormComponent {}
+export class ContractFormComponent {
+  djName = '';
+  djEmail = '';
+  djPhone = '';
+  clientName = '';
+  clientEmail = '';
+  clientPhone = '';
+  eventDate!: Date;
+  eventLocation = '';
+  music = false;
+  lighting = false;
+  soundEquipment = false;
+  fee = 0;
+  paymentTerms = '';
+
+  onSubmit() {
+    // Generate the contract document using the form values
+  }
+}
