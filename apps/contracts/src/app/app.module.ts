d2b3704988c48a420b3c44968d416d0e648c3547
Jason Ruhlin <jsonruhls@users.noreply.github.com>
2023-01-10 15:37:42 -0500

    add contract form component

--- apps/contracts/src/app/app.module.ts
+++ apps/contracts/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContractFormComponent } from './contract-form/contract-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ContractFormComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
